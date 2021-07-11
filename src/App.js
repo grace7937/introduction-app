5e90a9b0beee42bc36269410db4324413cf2ebf7
grace7937 <eunchong.k153@gmail.com>
2021-07-11 23:54:58 +0900

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
+import Title from "./components/Title";
+import Weather from "./components/Weather";
+import Todos from "./components/Todos";
+import { HashRouter, Route } from "react-router-dom";
+import { useState } from "react";
+import Aditting from "./routes/Aditting";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const changeTodos = (newTodos) => {
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <div>I'm ready</div>
-    </div>
+    <HashRouter>
+      <Route path="/" exact={true}>
+        <Title />
+        <Weather />
+        <Todos todos={todos} changeTodos={changeTodos} />
+      </Route>
+      <>
+        {todos.map((eachTodo) => {
+          return (
+            <Route path={"/" + eachTodo.id}>
+              <Aditting
+                todos={todos}
+                eachTodo={eachTodo}
+                changeTodos={changeTodos}
+              />
+            </Route>
+          );
+        })}
+      </>
+    </HashRouter>
   );
 }
-
 export default App;
